b1d2faaab303070ce91fd7ac557a1987b3f81d28
Dillon Lustick <lustickd@gmail.com>
2021-05-02 17:52:42 -0600

    Fix flickering/unusable items bug

---
+++ QuickSlots/Utility/NamesUtil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace QuickSlotsMod.Utility
+{
+    class NamesUtil
+    {
+        public static string[] slotNames = Enumerable.Range(1, Mod.config.SlotCount).Select(n => "QuickSlot" + n).ToArray();
+    }
+}
